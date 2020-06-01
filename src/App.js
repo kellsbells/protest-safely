2c2ffe592a4e3a471e4ec26eaf0d5ccd5ceff8ee
Vanessa Henson <vanessahenson@Vanessas-MacBook-Air.local>
2020-05-31 18:37:23 -0600

    Beginning of the react app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Mapcontainer from './map/map-container';
+
+function App() {
+  return (
+    <div className="App">
+      <Mapcontainer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/map/map-container.jsx
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import { Map, GoogleApiWrapper } from 'google-maps-react';
+import { googleMapsKey } from '../configs/config';
+import Body from '../map/body';
+// import * as scripts from './scripts';
+
+const mapStyles = {
+  width: '100%',
+  height: '100%'
+};
+
+export class MapContainer extends Component {
+    state = {
+       lat : 39.739235,
+       long: -104.990250,
+    }
+
+  render() {
+
+  
+
+    return (
+        <>
+    <Body />
+      <Map
+        google={this.props.google}
+        zoom={12}
+        style={mapStyles}
+        initialCenter={{
+         lat: this.state.lat,
+         lng: this.state.long,
+        }}
+      />
+      </>
+    );
+  }
+}
+
+//TODO figure out how to get this into secret key stuff for launch
+
+export default GoogleApiWrapper({
+  apiKey: googleMapsKey
+})(MapContainer);
